04d0a727e11958b0fe70e986d4501cd10775b106
Aaron Roberts <Lumbearjack@gmail.com>
2017-03-15 18:03:00 -0400

    fixed issues

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoListItem from './todolistitem.js';
 import { Router, Route, browserHistory } from 'react-router';
 
+class App extends React.Component {
+    constructor() {
+        super();
+    }
+    render(){
+        return (
+            <div>
+                <h1>Hello!</h1>
+            </div>
+        )
+    }
+    componentDidMount(){
+        console.log('Mounted');
+    }
+}
 ReactDOM.render(<App />, document.getElementById('app'));
