2db04e06f5b00c620651141ea7b97867dfd73774
Aaron Roberts <Lumbearjack@gmail.com>
2017-03-15 17:51:41 -0400

    removed duplicate app.js

--- src/app.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TodoListItem from './todolistitem.js';
-import { Router, Route, browserHistory } from 'react-router';
-
-ReactDOM.render(<App />, document.getElementById('app'));
